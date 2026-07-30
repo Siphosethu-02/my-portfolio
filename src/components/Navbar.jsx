4dbabc923459cd57aa0ac9334172ce48cc9b45b9
Siphosethu Dlamini <siphosethuthobelani4@gmail.com>
2026-07-30 11:36:47 +0200

    Updated portfolio styling and navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ function Navbar() {
     <nav>
       <h2>Siphosethu Thobelani Dlamini</h2>
 
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Contact</li>
-      </ul>
+    <ul>
+    <li><a href="#home">Home</a></li>
+    <li><a href="#about">About</a></li>
+    <li><a href="#skills">Skills</a></li>
+    <li><a href="#projects">Projects</a></li>
+    <li><a href="#contact">Contact</a></li>
+    </ul>
     </nav>
   );
 }
